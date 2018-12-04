536cfe7419a1fc2f41e5fa32907842b0bb7fd523
leojia <leojia@bu.edu>
2018-12-04 16:24:27 -0500

    Draw a second rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate sdl2;
 
+
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::render::{Texture, TextureCreator};
+
 use std::time::Duration;
 use std::thread::sleep;
 
@@ -22,6 +26,18 @@ pub fn main() {
 
 	// convert the window to canvas for display
 	let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+
+	let texture_creator: TextureCreator<_> = canvas.texture_creator();
+
+	const TEXTURE_SIZE: u32 = 32;
+
+	let mut square_texture: Texture = texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
+		.expect("Failed to create a texture");
+
+	canvas.with_texture_canvas(&mut square_texture, |texture| {
+		texture.set_draw_color(Color::RGB(0, 255, 0));
+		texture.clear();
+	}).expect("Failed to color a texture");
 
 	// draw on window
 	canvas.set_draw_color(Color::RGB(255, 0, 0));
@@ -44,6 +60,19 @@ pub fn main() {
 			}
 		}
 
+		canvas.set_draw_color(Color::RGB(255, 0, 0));
+		canvas.clear();
+
+		canvas.copy(
+			&square_texture,
+			None,		
+			Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE)
+		)
+		.expect("Couldn't copy texture into window.");
+
+		canvas.present();
+		
 		sleep(Duration::new(0, 1_000_000_000u32/60));
 	}
+
 }
