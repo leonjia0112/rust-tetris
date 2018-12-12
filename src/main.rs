62cbc5eaedee45c2d405b7607c6742f0a7c8a9d2
leojia <leojia@bu.edu>
2018-12-12 15:38:06 -0500

    Add Tetrimino element

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate sdl2;
+extern crate rand;
 
 
 use sdl2::pixels::Color;
@@ -10,6 +11,7 @@ use sdl2::render::{Texture, TextureCreator};
 use std::time::Duration;
 use std::thread::sleep;
 use sdl2::video::{Window, WindowContext};
+use sdl2::render::Canvas;
 
 #[derive(Clone, Copy)]
 enum TextureColor{
@@ -17,13 +19,200 @@ enum TextureColor{
 	Blue,
 }
 
+// 0 0 0 0
+// 0 0 0 0
+// 1 1 0 0
+// 1 1 0 0
+type Piece = Vec<Vec<u8>>;
+
+type States = Vec<Piece>;
+
+struct Tetrimino {
+	states: States,
+	x: isize,
+	y: usize,
+	current_state: u8,
+}
+
+trait TetriminoGenerator {
+	fn new() -> Tetrimino;
+}
+
+struct TetriminoI;
+
+impl TetriminoGenerator for TetriminoI {
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![1, 1, 1, 1],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![0, 1, 0, 0],
+						 	 vec![0, 1, 0, 0],
+						 	 vec![0, 1, 0, 0],
+						 	 vec![0, 1, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoS;
+
+impl TetriminoGenerator for TetriminoS {
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![0, 5, 5, 0],
+						 	 vec![5, 5, 0, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![5, 0, 0, 0],
+						 	 vec![5, 5, 0, 0],
+						 	 vec![0, 5, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoZ;
+
+impl TetriminoGenerator for TetriminoZ {
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![0, 6, 0, 0],
+						 	 vec![6, 6, 0, 0],
+						 	 vec![6, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![6, 6, 0, 0],
+						 	 vec![0, 6, 6, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoO;
+
+impl TetriminoGenerator for TetriminoO {
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![4, 4, 0, 0],
+						 	 vec![4, 4, 0, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoJ;
+
+impl TetriminoGenerator for TetriminoJ{
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![2, 2, 2, 0],
+						 	 vec![0, 0, 2, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![0, 2, 0, 0],
+						 	 vec![0, 2, 0, 0],
+						 	 vec![2, 2, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![2, 0, 0, 0],
+						 	 vec![2, 2, 2, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![2, 2, 0, 0],
+						 	 vec![2, 0, 0, 0],
+						 	 vec![2, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoL;
+
+impl TetriminoGenerator for TetriminoL{
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![0, 0, 3, 0],
+						 	 vec![3, 3, 3, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![3, 3, 0, 0],
+						 	 vec![0, 3, 0, 0],
+						 	 vec![0, 3, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![3, 3, 3, 0],
+						 	 vec![3, 0, 0, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![3, 0, 0, 0],
+						 	 vec![3, 0, 0, 0],
+						 	 vec![3, 3, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+struct TetriminoT;
+
+impl TetriminoGenerator for TetriminoT{
+	fn new() -> Tetrimino {
+		Tetrimino {
+			states: vec![
+						vec![vec![0, 7, 0, 0],
+						 	 vec![7, 7, 7, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![7, 0, 0, 0],
+						 	 vec![7, 7, 0, 0],
+						 	 vec![7, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![7, 7, 7, 0],
+						 	 vec![0, 7, 0, 0],
+						 	 vec![0, 0, 0, 0],
+						 	 vec![0, 0, 0, 0]],
+						vec![vec![0, 7, 0, 0],
+						 	 vec![7, 7, 0, 0],
+						 	 vec![0, 7, 0, 0],
+						 	 vec![0, 0, 0, 0]]],
+			x: 4,
+			y: 0,
+			current_state: 0,
+		}
+	}
+}
+
+
 fn create_texture_rect<'a>(canvas: &mut Canvas<Window>, 
 	texture_creator: &'a TextureCreator<WindowContext>, 
 	color: TextureColor, size:u32) -> Option<Texture<'a>> {
 	if let Ok(mut square_texture) = texture_creator.create_texture_target(None, size, size) {
 		canvas.with_texture_canvas(&mut square_texture, |texture| {
 			match color {
-				TexutreColor::Green => texture.set_draw_color(Color::RGB(0, 255, 0)),
+				TextureColor::Green => texture.set_draw_color(Color::RGB(0, 255, 0)),
 				TextureColor::Blue => texture.set_draw_color(Color::RGB(0, 0, 255)),
 			}
 			texture.clear();
